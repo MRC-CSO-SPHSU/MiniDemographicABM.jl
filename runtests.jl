4fda4373fdf9a93259405a0638010058eefd7a4d
Atiyah <a.m.g.elsheikh@gmail.com>
2023-02-10 10:18:03 +0000

    initial unit tests

---
+++ runtests.jl
@@ -0,0 +1,74 @@
+"""
+Run this script from shell as
+# JULIA_LOAD_PATH="/path/to/MiniDemographicABM.jl/src:\$JULIA_LOAD_PATH" julia RunTests.jl
+
+or within REPL
+
+julia> push!(LOAD_PATH,"/path/to/MiniDemographicABM.jl")
+julia> include("runtests.jl")
+"""
+
+using Agents
+using Test
+include("src/models.jl")
+
+
+@testset "MiniDemographicABM Testing" begin
+
+    @testset verbose=true "exploring Agents.jl functionalities" begin
+
+        towns = [ Town("A", 0.9, (1,1), House[]),
+                    Town("B", 0.3, (10,5), House[]),
+                    Town("C", 0.5, (4,6), House[]),
+                    Town("D", 0.7, (2,2), House[]) ]
+
+        maxTownGridDim = 10
+        space = CountryMap("WaqWaq",maxTownGridDim,towns)
+
+        @test length(space.towns) > 0
+
+        nhouses = 100
+
+        model = ABM(Person,space;properties=DemographyPars(initialPop=100))
+        @test length(positions(model)) == 0
+
+        houses = create_empty_houses!(space,nhouses)
+
+        @test length(houses) == 100
+        @test length(allhouses(model.space.towns)) == 100
+        @test length(positions(model)) == 100
+
+        seed!(model,floor(Int,time()))
+        @test model.initialPop == 100
+
+        person1 = add_agent_pos!(Person(1,houses[1]),model)
+        @test home(person1) == houses[1]
+
+        add_agent!(Person(nextid(model),UNDEFINED_HOUSE),houses[2],model)
+        @test home(model[2]) == houses[2]
+
+        add_agent!(model)
+        @test !ishomeless(model[3])
+
+        add_agent!(model)
+        @test nagents(model) == 4
+
+        add_agent!(houses[3],model)
+        @test home(model[5]) == houses[3]
+        @test length(allagents(model)) == 5
+
+        kill_agent!(model[1],model)
+        @test_throws KeyError model[1] == person1
+        kill_agent!(2,model)
+        @test nagents(model) == 3
+
+        @test model[4].id == 4
+        @test random_agent(model).id in [3,4,5]
+        @test sum(allids(model)) == 12
+
+        @test length(positions(model)) == 100
+        @test has_empty_positions(model)
+        @test !undefined(random_position(model))
+    end # Exploring Agents.jl
+
+end #
