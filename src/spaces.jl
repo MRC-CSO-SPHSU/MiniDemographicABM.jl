a5f979340882a6162f6af6083c0db6cb13be171d
Atiyah <a.m.g.elsheikh@gmail.com>
2023-02-11 10:35:25 +0000

    renaming

--- src/spaces.jl
+++ src/spaces.jl
@@ -3,24 +3,24 @@ space types for human populations
 """
 
 abstract type PopulationSpace <: Agents.DiscreteSpace end
-struct CountryMap <: PopulationSpace
+struct DemographicMap <: PopulationSpace
     countryname::String
     maxTownGridDim::Int
     towns::Vector{Town}
 end
 
-CountryMap(name,mtgd) = CountryMap(name,mtgd,Town[])
+DemographicMap(name,mtgd) = DemographicMap(name,mtgd,Town[])
 
-@delegate_onefield(CountryMap, towns, [random_town])
+@delegate_onefield(DemographicMap, towns, [random_town])
 
 # could be useful to forward delegate functions with arguments of space.towns
 
-function add_empty_house!(space::CountryMap)
+function add_empty_house!(space::DemographicMap)
     town = random_town(space)
     location = (rand(1:space.maxTownGridDim),rand(1:space.maxTownGridDim))
     return add_empty_house!(town,location)
 end
-function add_empty_houses!(space::CountryMap,nhouses)
+function add_empty_houses!(space::DemographicMap,nhouses)
     houses = House[]
     for _ in 1:nhouses
         house = add_empty_house!(space)
