17829d37116d7954c565d1a70e58a414a42b2ecd
Atiyah <a.m.g.elsheikh@gmail.com>
2023-02-12 09:52:48 +0000

    add_town! + idiomatic code

--- src/spaces.jl
+++ src/spaces.jl
@@ -21,13 +21,13 @@ DemographicMap(name,mtgd) = DemographicMap(name,mtgd,Town[])
 
 # could be useful to forward delegate functions with arguments of space.towns
 
-function add_empty_house!(space::DemographicMap)
+function add_empty_house!(space)
     town = random_town(space)
     location = (rand(1:space.maxTownGridDim),rand(1:space.maxTownGridDim))
     return add_empty_house!(town,location)
 end
 
-function add_empty_houses!(space::DemographicMap,nhouses)
+function add_empty_houses!(space,nhouses)
     houses = House[]
     for _ in 1:nhouses
         house = add_empty_house!(space)
@@ -35,3 +35,5 @@ function add_empty_houses!(space::DemographicMap,nhouses)
     end
     return houses
 end
+
+add_town!(space,density,location) = push!(space.towns,Town(density,location))
