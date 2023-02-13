cd760c7715e7ce839e86df270baa5c6167d47459
Atiyah <a.m.g.elsheikh@gmail.com>
2023-02-13 14:36:39 +0000

    better implementation of noperson

--- src/types/person.jl
+++ src/types/person.jl
@@ -4,26 +4,33 @@ include("../util.jl")
 @enum Gender male female
 random_gender() = rand((male,female))
 
+function noperson(::Type{HouseType}) where HouseType end
+
 mutable struct PersonH{HouseType} <: AbstractAgent
     id::Int
     pos::HouseType
     const gender::Gender
     age::Rational{Int}
     partner::PersonH{HouseType}
+    #=father::PersonH{HouseType}
+    mother::PersonH{HouseType}
+    children::PersonH{HouseType}=#
     function PersonH{HouseType}(id,pos,gender,age) where HouseType
         person = new{HouseType}(id,pos,gender,age)
+        person.partner = person.id == 0 ? person : noperson(HouseType)
         add_occupant!(pos,person)
         return person
     end
 end
 
-
 PersonH{HouseType}(id, pos; age, gender = random_gender()) where HouseType =
     PersonH{HouseType}(id, pos, gender, age)
 
-
 age(person) = person.age
 gender(person) = person.gender
+#=father(person) = isdefined(person,:father) ? person.father : NOPERSON
+mother(person) = isdefined(person,:mother) ? person.mother : NOPERSON=#
+
 
 home(person) = person.pos
 hometown(person) = person.pos.town
@@ -33,7 +40,7 @@ isfemale(person) = person.gender == female
 isadult(person) = person.age >= 18
 ischild(person) = person.age < 18
 partner(person) = person.partner
-issingle(person) = !isdefined(person,:partner) || partner(person) === NOPERSON
+issingle(person) =  partner(person) === NOPERSON # !isdefined(person,:partner) ||
 
 function reset_partnership!(person)
     if !issingle(person)
