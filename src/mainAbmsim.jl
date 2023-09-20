9c39ba803c2f10a4e70d63fd67de70fbe8518bfc
Atiyah <a.m.g.elsheikh@gmail.com>
2023-09-20 17:42:54 +0100

    initial main based on ABMSim.jl

--- src/mainAbmsim.jl
+++ src/mainAbmsim.jl
@@ -11,3 +11,16 @@ julia> include("script-name.jl")
 
 include("util.jl")
 add_to_loadpath!(pwd() * "/../../ABMSim.jl")
+
+using ABMSim: ABMSIMVERSION, init_abmsim
+using ABMSim: ABMSimulator, SimpleABMS
+include("./modelspec.jl")
+
+@assert ABMSIMVERSION == v"0.6.1"
+init_abmsim()  # reset agents id counter
+
+const simulator =
+    ABMSimulator(dt=1, starttime=2020//1, finishtime=2030//1, seed = 1, setupEnabled=false)
+
+const ukmap = declare_UK_map()
+const model = SimpleABMS{Person,DemographicMap}(ukmap)
