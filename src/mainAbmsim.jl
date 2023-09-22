da5227a817f097978f0b6da9d670a16b354d3d69
Atiyah <a.m.g.elsheikh@gmail.com>
2023-09-22 17:31:46 +0100

    declaration and initialization

--- src/mainAbmsim.jl
+++ src/mainAbmsim.jl
@@ -13,14 +13,41 @@ include("util.jl")
 add_to_loadpath!(pwd() * "/../../ABMSim.jl")
 
 using ABMSim: ABMSIMVERSION, init_abmsim
-using ABMSim: ABMSimulator, SimpleABMS
+using ABMSim: ABMSimulator, ABMPDVS
+using ABMSim: attach_agent_step!, step!
+
 include("./modelspec.jl")
 
 @assert ABMSIMVERSION == v"0.6.1"
 init_abmsim()  # reset agents id counter
 
+# Try without a simulator!
 const simulator =
-    ABMSimulator(dt=1, starttime=2020//1, finishtime=2030//1, seed = 1, setupEnabled=false)
+    ABMSimulator(dt=1//1, starttime=2020//1, finishtime=2030//1, seed = 1, setupEnabled=false)
 
+# Construct the model
+
+const pars = DemographyPars(initialPop = 10000)
+const data = DemographyData()
 const ukmap = declare_UK_map()
-const model = SimpleABMS{Person,DemographicMap}(ukmap)
+#const model = SimpleABMS{Person,DemographicMap}(ukmap)
+const model = ABMPDVS{Person,DemographyPars,DemographyData,Nothing,DemographicMap}(
+    pars,data,ukmap)
+parameters(model) = model.parameters
+
+# declaration of model components
+declare_population!(model,simulator)
+
+# Initialzation of model
+init_kinship!(model)
+
+# Step functions
+
+# attach_agent_step!(simulator,age_step!)
+
+# step
+
+# step!(model,simulator)
+# add_agent_pos!
+
+# Run
