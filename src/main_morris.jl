f206c69f6339c4cec22e94bb74411c09a9766c66
Atiyah <a.m.g.elsheikh@gmail.com>
2023-11-03 16:05:19 +0000

    reorganiztaion

--- src/main_morris.jl
+++ src/main_morris.jl
@@ -14,49 +14,18 @@ using Random
 
 include("./simspec.jl")
 
-#################
-# Step I - model
-#################
-# Model declaration, initialization and stepping!
-# With the functions below, simulation of the ABM model with Agents.jl can be conducted,
-#   see main.jl
+#=
+###########################################
+# Step I - model and simulation definitions
+###########################################
+model declaration, initializtaion and stepping definitions can be accessed in simspec.jl
+via the calls
+   declare_initialized_UKModel(..)
+   agent_steps()
+   model_steps()
 
-declare_model_properties(clock,starttime,initialPop,seednum;
-    startMarriedRate=0.8,
-    maxNumberOfMarriageCand=100,
-    baseDieRate = 0.0001,
-    maleAgeDieRate = 0.00021,
-    maleAgeScaling = 14.0,
-    femaleAgeDieRate = 0.00019,
-    femaleAgeScaling = 15.5,
-    basicDivorceRate = 0.06,
-    basicMaleMarriageRate = 0.7) =
-        DemographicABMProp{clock}(;starttime,initialPop,seednum,
-            startMarriedRate,maxNumberOfMarriageCand,
-            baseDieRate,maleAgeDieRate,maleAgeScaling,
-            femaleAgeDieRate,femaleAgeScaling,
-            basicDivorceRate,basicMaleMarriageRate)
-
-function declare_initialized_UKmodel(clock,properties)
-    model = UKDemographicABM(properties)
-    model.seednum == 0 ? Random.seed!(floor(Int,time())) : Random.seed!(model.seednum)
-    declare_population!(model)
-    init_kinship!(model) # the kinship among population
-    init_housing!(model) # housing assoication to population
-    return model
-end
-
-function agent_steps!(person,model)
-    age_step!(person,model)
-    death!(person,model)
-    divorce!(person,model)
-end
-
-function model_steps!(model)
-    metastep!(model) # incrementing time
-    dobirths!(model)
-    domarriages!(model)
-end
+   How to execute an ABM simulation based on Agents.jl, see main.jl
+=#
 
 #############################
 # Step II - active parameters
@@ -73,10 +42,15 @@ mutable struct ActiveParameter{ValType}
     end
 end
 setParValue!(model,activePar,val) = setfield!(model, activePar.name, val)
+function sample_parameters(apars)
+    pars = zeros(length(apars))
+    for (i,ap) in enumerate(apars)
+        pars[i] =  rand(Uniform(ap.lowerbound,ap.upperbound))
+    end
+    return pars
+end
 
 const startMarriedRate = ActiveParameter{Float64}(0.25,0.9,:startMarriedRate)
-
-# death parameters / yearly comulative (adhoc no model identification conducted)
 const baseDieRate = ActiveParameter{Float64}(0.00005,0.00015,:baseDieRate)
 const femaleAgeDieRate = ActiveParameter{Float64}(0.0001,0.0003,:femaleAgeDieRate)
 const femaleAgeScaling = ActiveParameter{Float64}(13.0,33.0,:femaleAgeScaling)
@@ -89,7 +63,7 @@ const basicMaleMarriageRate = ActiveParameter{Float64}(0.1,0.9,:basicMaleMarriag
 ##################################
 # Step III - Input/Output function
 ##################################
-## Define a simple simulation function of the form y = f(x)
+## Define a simple simulation-based function of the form y = f(x)
 ##  output  : the average age of the living population
 ##  input   : selected model parameters w.r.t. SA is sought
 ##
@@ -101,8 +75,8 @@ const ACTIVEPARS = [ startMarriedRate, baseDieRate, femaleAgeDieRate,femaleAgeSc
     maleAgeDieRate, maleAgeScaling, basicDivorceRate, basicMaleMarriageRate ]
 const CLOCK = Monthly
 const STARTTIME = 1951
-const NUMSTEPS = 12 * 200  # 100 year
-const INITIALPOP = 1000
+const NUMSTEPS = 12 * 100  # 100 year
+const INITIALPOP = 3000
 const SEEDNUM = 1
 SIMCNT::Int = 0
 LASTPAR::Vector{Float64} = []
@@ -119,7 +93,9 @@ function avg_livings_age(pars)
     for (i,p) in enumerate(pars)
         @assert ACTIVEPARS[i].lowerbound <= p <= ACTIVEPARS[i].upperbound
     end
-    properties = declare_model_properties(CLOCK,STARTTIME,INITIALPOP,SEEDNUM)
+    properties = DemographicABMProp{CLOCK}(starttime = STARTTIME,
+        initialPop = INITIALPOP,
+        seednum = SEEDNUM)
     for (i,p) in enumerate(pars)
         setParValue!(properties,ACTIVEPARS[i],p)
     end
@@ -138,21 +114,16 @@ end
 # Given the set of selected active parameters, their lower and upper bounds,
 #  generate a sample parameter set using a uniform distribution / just for testing
 
-function sample_parameters()
-    pars = zeros(length(ACTIVEPARS))
-    for (i,ap) in enumerate(ACTIVEPARS)
-        pars[i] =  rand(Uniform(ap.lowerbound,ap.upperbound))
-    end
-    return pars
-end
+# TODO .. this is left to the default conducted by the method implementation below
 
 ########################################
 # Step V - Perform SA using Morris method
 #########################################
 
+
 lbs = [ ap.lowerbound for ap in ACTIVEPARS ]
 ubs = [ ap.upperbound for ap in ACTIVEPARS ]
 
-res = gsa(  avg_livings_age,
+@time res = gsa(  avg_livings_age,
             Morris(relative_scale=true),
             [ [lbs[i],ubs[i]] for i in 1:length(ubs) ] )
--- src/simspec.jl
+++ src/simspec.jl
@@ -3,3 +3,32 @@ include("./spec/models.jl")
 include("./spec/declare.jl")
 include("./spec/initialize.jl")
 include("./spec/stepping.jl")
+
+
+#############################################################
+# model declaration, initializtaion and stepping definitions
+#############################################################
+#  Suggested agent_steps and model_steps for Agents.jl
+#
+
+
+function declare_initialized_UKmodel(clock,properties)
+    model = UKDemographicABM(properties)
+    model.seednum == 0 ? Random.seed!(floor(Int,time())) : Random.seed!(model.seednum)
+    declare_population!(model)
+    init_kinship!(model) # the kinship among population
+    init_housing!(model) # housing assoication to population
+    return model
+end
+
+function agent_steps!(person,model)
+    age_step!(person,model)
+    death!(person,model)
+    divorce!(person,model)
+end
+
+function model_steps!(model)
+    metastep!(model) # incrementing time
+    dobirths!(model)
+    domarriages!(model)
+end
