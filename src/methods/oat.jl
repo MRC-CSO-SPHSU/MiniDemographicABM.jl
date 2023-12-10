46b53ac2ad40451afcef7f61c5f10dd448779ccf
Atiyah <a.m.g.elsheikh@gmail.com>
2023-12-10 10:25:59 +0000

    solve API for std normalization alg

--- src/methods/oat.jl
+++ src/methods/oat.jl
@@ -21,9 +21,9 @@ struct NoNormalization  <: NormalizationAlg end
 struct ValNormalization <: NormalizationAlg end
 struct StdNormalization <: NormalizationAlg end
 
-ΔfΔp(f,p,δ::Float64,rmode ::RunMode,nalg::NormalizationAlg;seednum) =
+ΔfΔp(f,p,δ,rmode ::RunMode,nalg::NormalizationAlg;seednum) =
     notimplemented("ΔfΔp with run mode $typeof(rmode) and normalization alg $typeof(nalg) not implemented")
-ΔfΔp(f,p,δ::Float64,::MethodMultiRun,nalg::NormalizationAlg;seednum,mruns) =
+ΔfΔp(f,p,δ,::MethodMultiRun,nalg::NormalizationAlg;seednum,mruns) =
     notimplemented("ΔfΔp with multiple run mode and normalization alg $typeof(nalg) not implemented")
 
 "approximation of parameter sensitivities for a vector- (single-valued) function"
@@ -47,7 +47,7 @@ function ΔfΔp(f,pnom,δ::Float64,
     return ΔyΔp, ynom, yδall
 end
 
-function _normalize(ΔyΔp, ynom, pnom, ::ValNormalization)
+function _normalize(ΔyΔp, pnom, ynom, ::ValNormalization)
     ΔyΔpNorm = copy(ΔyΔp)
     ny = length(ynom)
     @simd for i in 1:ny
@@ -59,16 +59,34 @@ end
 "value normalized parameter sensitivities"
 function ΔfΔp(f,pnom,δ::Float64,::SingleRun,::ValNormalization; seednum)
     ΔyΔp , ynom, yδall = ΔfΔp(f,pnom,δ;seednum)
-    ΔyΔpNorm = _normalize(ΔyΔp,ynom,pnom,ValNormalization())
+    ΔyΔpNorm = _normalize(ΔyΔp,pnom,ynom,ValNormalization())
     return ΔyΔpNorm, ΔyΔp, ynom, yδall
 end
 
-function _normalize_std!(ΔyΔpNorm,σp,σy)
-    ny = size(ΔyΔpNorm)[1]
+"normalization with standard diviation of outputs and parameters"
+function _normalize(ΔyΔp,σp,σy,::StdNormalization)
+    ΔyΔpNor = copy(ΔyΔp)
+    ny = size(ΔyΔpNor)[1]
     @simd for i in 1:ny
-        @inbounds ΔyΔpNorm[i,:] =  σp .* ( ΔyΔpNorm[i,:] / σy[i] )
+        @inbounds ΔyΔpNor[i,:] =  σp .* ( ΔyΔpNor[i,:] / σy[i] )
     end
-    nothing
+    return ΔyΔpNor
+end
+
+"Evaluate stdandard diviation of function inputs and outputs"
+function evaluate_stddiv(f, ny, actpars, seednum,
+                            n=length(actpars)*length(actpars), sampleAlg = SobolSample())
+
+    σp = std(actpars)
+    pmatrix = sample(n,actpars,sampleAlg)  # design matrix
+    ymatrix = Array{Float64}(undef,ny,n)
+     # compute σ_y
+    @threads for i in 1:n
+        myseed!(seednum)
+        @inbounds ymatrix[:,i] = f(pmatrix[:,i])
+    end
+    σy = [std(ymatrix[i,:]) for i in 1:ny]
+    return σp, σy, pmatrix, ymatrix
 end
 
 """
@@ -76,47 +94,15 @@ parameter sensitivities normalized with standard diviations
     - of parameters derived from a uniform distribution
     - of model outputs computed via a design matrix
 """
-function ΔfΔp_normstd(f,p,δ,::RunMode=SingleRun();
-                        seednum,sampleAlg=SobolSample(),n=length(p)*length(p))
-    # compute derivatives
-    ΔyΔpNorm, y = ΔfΔp(f,p,δ;seednum)
-    ny = length(y)
+function ΔfΔp(f,pnom,δ::Float64,::SingleRun,::StdNormalization;seednum, σp, σy)
+                #actpars,seednum,sampleAlg=SobolSample(),n=length(p)*length(p))
+    # approximate derivatives
+    ΔyΔp, ynom, yδall = ΔfΔp(f,pnom,δ;seednum)
     # normalization
-    σp = std(actpars)
-    pmatrix = sample(n,actpars,sampleAlg)  # design matrix
-    ymatrix = Array{Float64}(undef,ny,n)
-     # compute σ_y
-    @threads for i in 1:n
-        myseed!(seed!)
-        @inbounds ymatrix[:,i] = f(pmatrix[:,i])
-    end
-    σy = [std(ymatrix[i,:]) for i in 1:ny]
-    _normalize_std!(ΔyΔpNorm,σp,σy)
-    return  ΔyΔpNorm, y, ymatrix, σy, σp
+    ΔyΔpNor = _normalize(ΔyΔp,σp,σy,StdNormalization())
+    return  ΔyΔpNor, ΔyΔp, ynom, yδall
 end
 
-
-function ΔfΔp_normstd(f,p,δ,::MethodMultiRun;
-    seednum,mruns,sampleAlg=SobolSample(),n=length(p)*length(p))
-
-    ΔyΔpNorm, y, _, σy, σp = ΔfΔp_normstd(f,p,δ;seednum,sampleAlg,n)
-
-    ny = length(y)
-    yall = Array{Float64,2}(undef,ny,mruns)
-    yall[:,1] = y
-
-    # Multi-level multi-threading
-    addlock = ReentrantLock()
-    @threads for i in 2:mruns
-        tmp, yall[:,i] = ΔfΔp(f,p,δ;seednum=seednum+i-1)
-        _normalize_std!(tmp,σp,σy)
-        @lock addlock ΔyΔpNorm += tmp
-    end
-    yavg = sum(yall,dims = 2) / mruns
-    ΔyΔpNorm /= mruns
-
-    return ΔyΔpNorm, yall, yavg, σy, σp
-end
 
 """
 OAT Result contains:
@@ -144,15 +130,28 @@ mutable struct OATResult
         normalize!(oatres,ValNormalization())
         return oatres
     end
+
+    function OATResult(f, actpars, δ, ::StdNormalization; σp, σy, kwargs...)
+        oatres = OATResult(f,actpars,δ,NoNormalization(); kwargs...)
+        # σp, σy = evaluate_stddiv(f, length(oatres.ynom), actpars, seednum, n, samlpleAlg)
+        normalize!(oatres,σp,σy,StdNormalization())
+        return oatres
+    end
+
 end # OATResult
 
 function normalize!(oatres::OATResult,::ValNormalization)
-    oatres.∂y∂pNor = _normalize(oatres.∂y∂p, oatres.ynom, oatres.pnom, ValNormalization())
+    oatres.∂y∂pNor = _normalize(oatres.∂y∂p, oatres.pnom,  oatres.ynom, ValNormalization())
+    return nothing
+end
+
+function normalize!(oatres::OATResult, σp, σy, ::StdNormalization)
+    oatres.∂y∂pNor = _normalize(oatres.∂y∂p, σp, σy, StdNormalization())
     return nothing
 end
 
 solve(::OATProblem, f, actpars::Vector{ActiveParameter{Float64}}, ::SingleRun;
-    δ, normAlg::NormalizationAlg = NoNormalization(), seednum) =
-        OATResult(f, actpars, δ, normAlg; seednum)
+    δ, normAlg::NormalizationAlg = NoNormalization(), kwargs...) =
+        OATResult(f, actpars, δ, normAlg; kwargs...)
 
 # normalize!(::OATResult,::ValNormalization)
